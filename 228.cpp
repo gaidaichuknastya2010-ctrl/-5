d386f75ea0c4a2de93725a53534fb965dd62a741
gaidaichuknastya2010-ctrl <gaidaichuknastya2010@gmail.com>
2026-06-27 13:02:02 +0300

    Add files via upload

---
+++ 228.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+
+    int level = 1;
+
+    cout << " гра: привид за дверми " << endl;
+    cout << " виберай двері пробуючи не натрапити на привида" << endl;
+
+    while (level <= 10) {
+        int g = rand() % 3 + 1;
+        int choice;
+
+        cout << "\n Рівень " << level << endl;
+
+        cout << "  _____     _____     _____ " << endl;
+        cout << " |     |   |     |   |     | " << endl;
+        cout << " |  0  |   |  0  |   |  0  | " << endl;
+        cout << " |--   |   |--   |   |--   | " << endl;
+        cout << " |     |   |     |   |     | " << endl;
+        cout << " |_____|   |_____|   |_____| " << endl;
+        cout << "    1         2         3    " << endl;
+
+
+        cout << "Вибери двері (1, 2 або 3): ";
+        cin >> choice;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Помилка! Введи число 1, 2 або 3." << endl;
+            continue;
+        }
+
+        if (1 > choice > 3) {
+            cout << "Немає таких дверей!" << endl;
+            continue;
+        }
+
+        if (choice == g) {
+            cout << "\n Привид! Рівень анульовано." << endl;
+            level = 1;
+        }
+        else {
+            cout << "\n за дверми " << choice << " порожні." << endl;
+            level++;
+        }
+    }
+
+    cout << "\nВІТАЮ! Ти вижив!" << endl;
+    return 0;
+}
---
+++ буль буль карасики.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+
+    int N = rand() % 1000 + 1;
+
+    int number;
+    int s = 0;
+    int maxs = 10;
+    int start = 0;
+
+    cout << "Гра відгадач число" << endl;
+
+    cout << "Почати гру? (0 - так, 1 - ні): ";
+    cin >> start;
+
+    if (start == 1) {
+        return 0;
+    }
+
+    else {
+
+        cout << "У тебе є всього 10 спроб щоб вгадати число від 1 до 1000." << endl;
+
+        while (s < maxs) {
+            cout << "\nЗалишилось спроб: " << (maxs - s) << endl;
+            cout << "Введи число: ";
+            cin >> number;
+
+            if (cin.fail()) {
+                cout << "Помилка! Вводь тільки цифри." << endl;
+                cin.clear();
+                cin.ignore(1000, '\n');
+                continue;
+            }
+
+            s = s + 1;
+
+            if (number > N) {
+                cout << "Забагато" << endl;
+            }
+            else if (number < N) {
+                cout << "Холодно. Спробуй більше число." << endl;
+            }
+            else {
+                cout << "Ти вгадав число за " << s << " спроб! Маладец" << endl;
+                return 0;
+            }
+        }
+
+        cout << "\nТи програв! Мухахаххах! Число було: " << N << endl;
+
+    }
+
+    return 0;
+}
